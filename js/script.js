61fa6b28d19fb4bcbaa16db3d0c66c25b74d63c4
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2021-09-07 21:23:58 +0200

    scale 100vh bien pointé

--- js/script.js
+++ js/script.js
@@ -5,8 +5,8 @@ gsap.config({force3D: false});
 let tl = gsap.timeline({
   scrollTrigger: {
     start: 0,
-    end: "max",
-    scrub: 2
+    end: "bottom",
+    scrub: 1
   }
 });
 
@@ -32,52 +32,52 @@ gsap.utils.toArray(".ink-box").forEach((section, i) => {
 
 
 
-let main = document.getElementById('ink');
-ScrollTrigger.create({
-  trigger: ".ink-a-js",
-  start: "top",
-  end: "bottom",
-  toggleClass: {targets: main, className: "a"},
-  //markers: true
-});
+// let main = document.getElementById('ink');
+// ScrollTrigger.create({
+//   trigger: ".ink-a-js",
+//   start: "top",
+//   end: "bottom",
+//   toggleClass: {targets: main, className: "a"},
+//   //markers: true
+// });
 
-ScrollTrigger.create({
-  trigger: ".ink-b-js",
-  start: "top+=100%",
-  end: "bottom+=100%",
-  toggleClass: {targets: main, className: "b"},
-  //markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".ink-b-js",
+//   start: "top+=100%",
+//   end: "bottom+=100%",
+//   toggleClass: {targets: main, className: "b"},
+//   //markers: true
+// });
 
-ScrollTrigger.create({
-  trigger: ".ink-c-js",
-  start: "top+=200%",
-  end: "bottom+=200%",
-  toggleClass: {targets: main, className: "c"},
-  //markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".ink-c-js",
+//   start: "top+=200%",
+//   end: "bottom+=200%",
+//   toggleClass: {targets: main, className: "c"},
+//   //markers: true
+// });
 
-ScrollTrigger.create({
-  trigger: ".ink-d-js",
-  start: "top+=300%",
-  end: "bottom+=300%",
-  toggleClass: {targets: main, className: "d"},
-  //markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".ink-d-js",
+//   start: "top+=300%",
+//   end: "bottom+=300%",
+//   toggleClass: {targets: main, className: "d"},
+//   //markers: true
+// });
 
-ScrollTrigger.create({
-  trigger: ".ink-e-js",
-  start: "top+=400%",
-  end: "bottom+=400%",
-  toggleClass: {targets: main, className: "e"},
-  //markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".ink-e-js",
+//   start: "top+=400%",
+//   end: "bottom+=400%",
+//   toggleClass: {targets: main, className: "e"},
+//   //markers: true
+// });
 
-ScrollTrigger.create({
-  trigger: ".ink-f-js",
-  start: "top+=500%",
-  end: "bottom+=500%",
-  toggleClass: {targets: main, className: "f"},
-  //markers: true
-});
+// ScrollTrigger.create({
+//   trigger: ".ink-f-js",
+//   start: "top+=500%",
+//   end: "bottom+=500%",
+//   toggleClass: {targets: main, className: "f"},
+//   //markers: true
+// });
 
