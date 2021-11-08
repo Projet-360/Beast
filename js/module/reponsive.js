f24fa3d0a9d1c4c43ccf08bf4cabf3e6a856f3d2
Pierre FABIEN <p.fabien@caplaser.fr>
2021-11-08 09:44:54 +0100

    disclaimer mobile DONE

--- js/module/reponsive.js
+++ js/module/reponsive.js
@@ -1,8 +1,20 @@
 // PERMET DE SUPPRIMER LES ETOILES POUR LES MOBILES
+// PERMET DE AFFICHER LE DISCLAIMER POUR LES MOBILES A BASSE PERFORMANCES
 // IL SUPPRIME LA BALISE EN DESSOUS DE 768PX
 const stars = document.getElementById('stars');
+const disclaimerMobile = document.getElementById('disclaimerMobile');
+const boutonContinue = document.getElementById('disclaimer_mobile_container_continue');
+
+boutonContinue.onclick = function() { 
+    disclaimer_mobile.style.visibility = "hidden";
+    disclaimer_mobile.style.opacity = "0";
+ };
+
 document.addEventListener("DOMContentLoaded", function (event) {
   if (window.matchMedia("(max-width: 768px)").matches) {
-    stars.remove();
+    stars.remove();    
+    
+    disclaimer_mobile.style.visibility = "visible";
+    disclaimer_mobile.style.opacity = "1";
   }
 });
