b62a1b775648faa0c1c19f6472be837bf61622f9
Pierre FABIEN <p.fabien@caplaser.fr>
2021-12-01 10:06:35 +0100

    favicon and sitemap

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 import './module/gsap.js'
 
-//import './module/langue.js'
-//import './module/changerLang.js'
+import './module/langue.js'
+import './module/changerLang.js'
 
-//import './module/load.js'
+import './module/load.js'
 
 import './module/import-svg.js'
 import './module/grained.js'
